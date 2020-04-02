1d70835a44d3dbbb869231701b5fe1856f4d1504
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-04-02 21:30:09 +0700

    Mengedit view admin
    hapus akun, info user, input score

---
+++ WPTTI/application/views/admininputscore.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8"> -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>/assets/css/bootstrap.css" type="text/css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/darkly/bootstrap.min.css"> -->
+    <title>TOEFL | Institut Teknologi Sumatera</title>
+
+  </head>
+  <body>
+    <div class="navi">
+      <img src="<?php echo base_url() ?>/assets/img/itera.png" alt="">
+        <h1>TOEFL ITERA</h1>
+        <ul>
+          <li><a href="#" style="padding-right: 0.5rem;">Logout</a></li>
+          <li><a href="#" style="padding-right: 0.5rem;">Tambah Nilai</a></li>
+          <li><a href="#" style="padding-right: 0.5rem;">Pengaturan Akun Pendaftar</a></li>
+          <li><a href="#" style="padding-right: 0.5rem;">Beranda</a></li>
+        </ul>
+      </div>
+      <div class="main">
+        <h2><center>Tambah Nilai</center></h2>
+  </body>
+</html>
