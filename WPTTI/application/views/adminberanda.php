3940958302c8e909fc8a8303e7c14e3d9470c988
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-04-05 23:15:23 +0700

    merubah halaman beranda admin
    berisi fungsi cari, list akun, fungsi input score, fungsi hapus akun

--- WPTTI/application/views/adminberanda.php
+++ WPTTI/application/views/adminberanda.php
@@ -13,47 +13,18 @@
         <h1>TOEFL ITERA</h1>
         <ul>
           <li><a href="<?= base_url('index.php/login');?> " style="padding-right: 0.5rem;">Logout</a></li>
-          <li><a href="<?= base_url('index.php/admininputscore');?> " style="padding-right: 0.5rem;">Tambah Score</a></li>
-          <li><a href="<?= base_url('index.php/admininfouser');?> " style="padding-right: 0.5rem;">Akun Pendaftar</a></li>
           <li><a href="<?= base_url('index.php/adminberanda');?> " style="padding-right: 0.5rem;">Beranda</a></li>
         </ul>
-      </div>
-      <div class="main">
-        <h2>Beranda Utama <br>
-           Pendaftaran Test TOEFL ITERA
-        </h2>
-        <center>
-          <p style="margin-left: 1rem; margin-top: 1rem">
-            We provide some courses for specific purpose, like conversation in English and TOEFL<br>
-            Preparation. We also provide some courses focused on improving your speaking, writing, <br>
-            listening and reading skills.
-          </p>
-        </center>
-        <img src="<?php echo base_url() ?>/assets/img/itera/2.jpg" alt="Los Angeles" style="width:38%;margin-left:auto; margin-right:auto; display: block;">
+    </div>
+    <div class="main">
+      <h2>Informasi Akun Pendaftar</h2>
+      <form action= "" method="get">
+        <input class="pencarian" type="text" name="keyword">
+        <input class="button" type="submit" name="" value="cari"><br><br>
+      </form>
+      <br>
+      <br>
+      <!-- DISINI ISINYA LIST AKUN, FUNGSI HAPUS AKUN, INPUT SOCRE-->
+    </div>
   </body>
 </html>
-<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <img src="">
-  <a class="navbar-brand">UPT Bahasa ITERA</a>
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
- </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav ml-auto" style="font-weight: bold">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pengaturan Akun Pendaftar</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Tambah Nilai TOEFL</a>
-      </li>
-
-
-    </ul>
-
-  </div>
-</nav> -->
